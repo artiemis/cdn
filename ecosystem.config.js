b32f18c369e57608f8492b1e0550c12cc2f22427
artiemis <artur8259@outlook.com>
2022-03-11 08:34:53 +0000

    enable hypercorn access logger

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
 		{
 			name: 'cdn',
 			script: 'env/bin/python',
-			args: '-m hypercorn main:app --bind "0.0.0.0:5000"',
+			args: '-m hypercorn main:app --bind "0.0.0.0:5000" --access-logfile -',
 		},
 	],
 };
